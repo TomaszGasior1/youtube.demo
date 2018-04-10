75df9f7df255eb163c36fd8bfc56a49a77167d18
Rizos <tomasz.gasior1990@gmail.com>
2018-04-10 19:55:21 +0200

    Changes to index

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,11 @@
 
 public class HelloWorld {
-	static int i=1;
-	char c= (char) 44;
-	Zm z=new Zm(i,c);
-		public static void main(String[] args) {
-		System.out.println(z.c);
+
+	public static void main(String[] args) {
+		int i = 1;
+		char c = (char) 47;
+		Zm z = new Zm(i, c);
+		System.out.println(z);
 	}
 
 }
--- src/Zm.java
+++ src/Zm.java
@@ -1,8 +1,15 @@
 
 public class Zm {
-	public int i;
-	public char c;
-	public Zm()
+	private int i;
+	private char c;
+	public Zm(int i, char c){
+		this.i = i;
+		this.c = c;
+	}
+	@Override
+	public String toString() {
+		return "Zm [i=" + i + ", c=" + c + "]";
+	}
 	
 	
 
