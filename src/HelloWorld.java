6b42476405cee04b58416d3c353638a36ce3eaa2
Rizos <tomasz.gasior1990@gmail.com>
2018-04-10 15:45:55 +0200

    start tracking src folder

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+	static int i=1;
+	char c= (char) 4;
+	Zm z=new Zm(i,c);
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(z.c);
+	}
+
+}
---
+++ src/Zm.java
@@ -0,0 +1,9 @@
+
+public class Zm {
+	public int i;
+	public char c;
+	public Zm()
+	
+	
+
+}
